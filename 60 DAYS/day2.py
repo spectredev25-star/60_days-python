25862237aabb1e1ef190a932d23a151b3f27cd27
SPECTRE <spectredev25@gmail.com>
2026-03-18 23:12:18 -0700

    Add files via upload
    ANOTHER EXCITING PROJECT I GAT MY HANDS ON,😏DAY2 AND COUNTING.....

--- 60 DAYS/day2.py
+++ 60 DAYS/day2.py
@@ -1,5 +1,13 @@
 import requests
 import sys
+
+weather_icons = {
+    'rain':'🌧',
+    'clear':'☀',
+    'clouds':'☁',
+    'snow':'❄',
+    'thunderstorm':'⛈'
+}
 def get_weather(city,api_key):
     BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
     params = {
@@ -15,11 +23,15 @@ def get_weather(city,api_key):
         description = data['weather'][0]['description'].capitalize()
         humidity = data['main']['humidity']
         wind_speed = data['wind']['speed']
+
+        weather = data['weather'][0]['main'].lower()
+        weather_icon = weather_icons.get(weather,'😀')
         
         print(f'TEMPERATURE IN {city} IS {temperature}C')
         print(f'DESCRIPTION: {description}')
         print(f'HUMIDITY: {humidity}%')
         print(f'WIND SPEED: {wind_speed}m/s')
+        print(f'IT IS {weather.upper()} TODAY {weather_icon}.')
     
     except requests.exceptions.HTTPError:
         print('CITY NOT FOUND OR INVALID API KEY')
